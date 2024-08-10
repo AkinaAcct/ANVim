db694385bb8a29088d60b9dd7e16302a4e601ac9
AkinaAcct <108107785+AkinaAcct@users.noreply.github.com>
2024-08-10 10:44:49 +0800

    livecode: set default username to 'git config user.name'

--- lua/plugins/livecode.lua
+++ lua/plugins/livecode.lua
@@ -1,8 +1,13 @@
 return {
     "jxm35/livecode.nvim",
     config = function ()
+        -- get current username
+        local git_username = vim.fn.system("git config user.name")
+        git_username = git_username:gsub("\n", "") -- remove \n
+
+        -- enable livecode
         require("livecode").setup({
-            username = "AkinaAcct",
+            username = git_username,
         })
     end,
 }
