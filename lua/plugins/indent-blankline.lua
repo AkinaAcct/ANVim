7528848e5ed3c211332ddc208357aa7dc6f70a15
AkinaAcct <108107785+AkinaAcct@users.noreply.github.com>
2024-11-11 00:24:28 +0800

    use Snacks.nvim
    snacks-nvim.lua

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -2,7 +2,7 @@ return {
   "lukas-reineke/indent-blankline.nvim",
   event = "LazyFile",
   opts = function()
-    LazyVim.toggle.map("<leader>ug", {
+    Snacks.toggle({
       name = "Indention Guides",
       get = function()
         return require("ibl.config").get_config(0).enabled
@@ -10,27 +10,29 @@ return {
       set = function(state)
         require("ibl").setup_buffer(0, { enabled = state })
       end,
-    })
+    }):map("<leader>ug")
 
     return {
       indent = {
         char = "│",
         tab_char = "│",
       },
-      scope = { show_start = true, show_end = false },
+      scope = { show_start = false, show_end = false },
       exclude = {
         filetypes = {
-          "help",
+          "Trouble",
           "alpha",
           "dashboard",
-          "neo-tree",
-          "Trouble",
-          "trouble",
+          "help",
           "lazy",
           "mason",
+          "neo-tree",
           "notify",
+          "snacks_notif",
+          "snacks_terminal",
+          "snacks_win",
           "toggleterm",
-          "lazyterm",
+          "trouble",
         },
       },
     }
--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -1,13 +1,13 @@
 return {
-    "folke/lazydev.nvim",
-    ft = "lua",
-    opts = function()
-        return {
-            library = {
-                uv = "luvit-meta/library",
-                lazyvim = "LazyVim",
-            },
-        }
-    end,
+  "folke/lazydev.nvim",
+  ft = "lua",
+  cmd = "LazyDev",
+  opts = {
+    library = {
+      { path = "luvit-meta/library", words = { "vim%.uv" } },
+      { path = "LazyVim", words = { "LazyVim" } },
+      { path = "snacks.nvim", words = { "Snacks" } },
+      { path = "lazy.nvim", words = { "LazyVim" } },
+    },
+  },
 }
-
